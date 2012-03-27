9a62ad8b52facf012fd20b87a0d4e4db38d1d8bb
sarun87@gmail.com <sarun87@gmail.com@310eed46-5812-bb6a-ce30-aefc55b4996e>
2012-03-27 16:08:37 +0000

    Pointer conversion logic. Code working. sandbox.c complete. TODO: Code commenting for trigger.c and sandbox.c and addition of team header.

--- trunk/src/sandbox.c
+++ trunk/src/sandbox.c
@@ -1,30 +1,25 @@
 #include <signal.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/mman.h>
 #include <sys/ucontext.h>
 
 #define REG_RSP 15
-#define REG_RIP 16
 
 extern void readString(char *s, int r);
 
 void signalFunc(int sig, siginfo_t *siginfo, void *ucp)
 {
-	static unsigned long long recordedAddress = 0;
-	static unsigned long long functionStartAddress = 0;
+	static unsigned long recordedAddress = 0;
+	static unsigned long functionStartAddress = 0;
 	if (recordedAddress == 0)
 	{
 		//Entering a function.
-		recordedAddress = (unsigned long long)((ucontext_t *) ucp)->uc_mcontext.gregs[REG_RSP];
-		functionStartAddress = (unsigned long long)((ucontext_t *) ucp)->uc_mcontext.gregs[REG_RIP];
-		//Unprotect the page corresponding to the SIGSEGV
-		//functionStartAddress = *(unsigned int *) siginfo->si_addr;
-		printf("\nSIGSEV - Entering a function (%u)",functionStartAddress);
-		printf("\nRecorded address - %x",recordedAddress);
-		printf("\nStack Pointer - %x",(unsigned long long)((ucontext_t *) ucp)->uc_mcontext.gregs[REG_RSP]);
-		printf("\nInstruction Pointer - %x",(unsigned long long)((ucontext_t *) ucp)->uc_mcontext.gregs[REG_RIP]);
-		printf("\nsiginfo->si_addr - %x",*((unsigned long long *) siginfo->si_addr));
+		recordedAddress  = (unsigned long) *(unsigned long**) ((ucontext_t *)ucp)->uc_mcontext.gregs[REG_RSP];
+		functionStartAddress = (unsigned long) siginfo->si_addr;
+		//printf("\nSIGSEV - Entering a function (%x)",functionStartAddress);
+		//printf("\nRecorded address - %x",recordedAddress);
 		if (mprotect((void*)(functionStartAddress&0xfffffffffffff000), 1,PROT_WRITE | PROT_EXEC | PROT_READ) == -1)
 		{
 			printf("\nmprotect failed for %u",functionStartAddress);
@@ -37,7 +32,7 @@ void signalFunc(int sig, siginfo_t *siginfo, void *ucp)
 			exit(0);
 		}
 	}
-	else if (recordedAddress == (unsigned long long)((ucontext_t *) ucp)->uc_mcontext.gregs[REG_RIP])
+	else if (recordedAddress == (unsigned long) *(unsigned long**)((ucontext_t *) ucp)->uc_mcontext.gregs[REG_RSP])
 	{
 		//Leaving a function.
 		printf("\nSIGSEV - Leaving a function (%u)",functionStartAddress);
@@ -47,11 +42,11 @@ void signalFunc(int sig, siginfo_t *siginfo, void *ucp)
 		mprotect((void*)(functionStartAddress & 0xfffffffffffff000), 1, PROT_NONE);
 		recordedAddress = 0;
 	}
-	//else
-	//{
-	//	printf("\nMismatch between recorded and obtained address\n");
-	//	exit(0);
-	//}
+	else
+	{
+		printf("malicious buffer overflow detected, will exit!\n");
+		exit(0);
+	}
 }
 
 //Init the signal handlers here
@@ -61,10 +56,10 @@ void init_sandbox()
 
 	sigemptyset(&segfaulthandler.sa_mask);
 	segfaulthandler.sa_flags = SA_RESTART | SA_SIGINFO;
-	segfaulthandler.sa_handler = signalFunc;
+	segfaulthandler.sa_sigaction = signalFunc;
 	sigaddset(&segfaulthandler.sa_mask,SIGSEGV);
 	sigprocmask(SIG_UNBLOCK,&segfaulthandler.sa_mask,NULL);
 	sigaction(SIGSEGV,&segfaulthandler,NULL);
 
-	mprotect((void*)((unsigned int)readString & 0xfffffffffffff000), 1,PROT_READ | PROT_WRITE/* | PROT_EXEC */); // cannot execute !!!
+	mprotect((void*)((unsigned long)readString & 0xfffffffffffff000), 1,PROT_READ | PROT_WRITE/* | PROT_EXEC */); // cannot execute !!!
 }
